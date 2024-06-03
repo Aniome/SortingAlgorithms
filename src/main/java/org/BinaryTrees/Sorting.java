6dbf892a72cd3250e9afd658167bc59c46dd6721
Aniome <vaganov.kostya2000@yandex.ru>
2024-06-03 07:47:51 +0500

    add bubble sort

---
+++ src/main/java/org/BinaryTrees/Sorting.java
@@ -0,0 +1,25 @@
+package org.BinaryTrees;
+
+public class Sorting {
+    public int[] array;
+
+    public Sorting(int size) {
+        array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+    }
+
+    public void bubbleSort() {
+        int length = array.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
